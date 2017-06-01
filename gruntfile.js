ec26a76c9e5823d934e8177711f0ab1c75725c01
iddiaz <diazdiazivan@gmail.com>
2017-06-01 04:09:56 +0200

    configurando minificadores js y sass

--- gruntfile.js
+++ gruntfile.js
@@ -30,13 +30,37 @@ module.exports = function (grunt) {
          options: {
             sourceMap: true
          }
+      },
+      uglify: {
+         dist: {
+            files: {
+               'dist/js/package.min.js': 'dist/js/**/*.js'
+            }
+         },
+         options: {
+            sourceMap: true,
+            sourceMapIn: 'dist/js/package.js.map'
+         }
+      },
+      cssmin: {
+         dist:{
+            files:{
+               'dist/styles/styles.min.css': 'dist/styles/**/*.css'
+            }
+         },
+         options: {
+            sourceMap: true
+         }
       }
+
    });
 
    grunt.loadNpmTasks('grunt-contrib-jshint');
    grunt.loadNpmTasks('grunt-contrib-coffee');
    grunt.loadNpmTasks('grunt-sass');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-   grunt.registerTask('default', ['jshint', 'coffee', 'sass'])
+   grunt.registerTask('default', ['jshint', 'coffee', 'sass', 'uglify', 'cssmin' ])
 
 }
